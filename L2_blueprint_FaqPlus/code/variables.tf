87a635257c027d391951d9a4dd089c59a7943784
nephinj <john.nephin@outlook.com>
2021-02-01 13:30:22 -0500

    automation of chatbot projects

---
+++ L2_blueprint_FaqPlus/code/variables.tf
@@ -0,0 +1,17 @@
+variable "tags" {}
+
+variable "env" {}
+variable "group" {}
+variable "project" {}
+
+variable "location" {}
+
+variable "L1_terraform_remote_state_config" {}
+
+#
+
+variable lowerlevel_storage_account_name {}
+variable lowerlevel_resource_group_name {}
+variable lowerlevel_container_name {}
+variable lowerlevel_key {}
+
---
+++ L2_blueprint_WSTE-KB/code/20-qnaservice.tf
@@ -0,0 +1,35 @@
+
+//Seems to only work with Windows.  Linux times out on the endpoint even after an hour
+//Only windows is available via the portal deploy from qnamaker.ai
+module "ScSc-CIO-Chatbot-WSTE-EN-KB" {
+  //  for_each                  = local.deployList
+  source                    = "github.com/canada-ca-terraform-modules/terraform-azurerm-qna-knowledgebase?ref=20201019.1"
+  location                  = local.resource_groups_L2.WSTE_KB.location
+  cognitiveServicesLocation = var.cognitiveServicesLocation
+  resourceGroupName         = local.resource_groups_L2.WSTE_KB.name
+  prefix                    = "${local.prefix}-${var.knowledgebaseList[0].languageCode}"
+  knowledgebaseList         = var.knowledgebaseList[0].knowledgebaseLocations
+  qna_tier                  = var.qna_tier
+  qna_size                  = var.qna_size
+  search_sku                = var.search_sku
+  account_sku               = var.account_sku
+  tags                      = var.tags
+  plan_id                   = azurerm_app_service_plan.Chatbot-svcplan.id
+}
+
+module "ScSc-CIO-Chatbot-WSTE-FR-KB" {
+  //  for_each                  = local.deployList
+  source                    = "github.com/canada-ca-terraform-modules/terraform-azurerm-qna-knowledgebase?ref=20201019.1"
+  location                  = local.resource_groups_L2.WSTE_KB.location
+  cognitiveServicesLocation = var.cognitiveServicesLocation
+  resourceGroupName         = local.resource_groups_L2.WSTE_KB.name
+  prefix                    = "${local.prefix}-${var.knowledgebaseList[1].languageCode}"
+  knowledgebaseList         = var.knowledgebaseList[1].knowledgebaseLocations
+  qna_tier                  = var.qna_tier
+  qna_size                  = var.qna_size
+  search_sku                = var.search_sku
+  account_sku               = var.account_sku
+  tags                      = var.tags
+  plan_id                   = azurerm_app_service_plan.Chatbot-svcplan.id
+}
+
