7541ce245702592f41256646788ffe410bf54ce4
nephinj <john.nephin@canada.ca>
2021-06-16 20:04:43 +0000

    added GWCC

---
+++ L2_blueprint_GWCC-KB/code/20-qnaservice.tf
@@ -0,0 +1,39 @@
+
+//Seems to only work with Windows.  Linux times out on the endpoint even after an hour
+//Only windows is available via the portal deploy from qnamaker.ai
+module "ScSc-CIO-Chatbot-IM-EN-KB" {
+  //  for_each                  = local.deployList
+  source                    = "github.com/canada-ca-terraform-modules/terraform-azurerm-qna-knowledgebase?ref=20210302.1"
+  location                  = local.resource_groups_L2.IM_KB.location
+  cognitiveServicesLocation = var.cognitiveServicesLocation
+  resourceGroupName         = local.resource_groups_L2.IM_KB.name
+  prefix                    = "${local.prefix}-${var.knowledgebaseList[0].languageCode}"
+  knowledgebaseList         = var.knowledgebaseList[0].knowledgebaseLocations
+  qna_tier                  = var.qna_tier
+  qna_size                  = var.qna_size
+  search_sku                = var.search_sku
+  account_sku               = var.account_sku
+  tags                      = var.tags
+  plan_id                   = azurerm_app_service_plan.Chatbot-svcplan.id
+  search_service            = var.search_service_en
+  search_service_key        = var.search_service_key_en
+}
+
+module "ScSc-CIO-Chatbot-IM-FR-KB" {
+  //  for_each                  = local.deployList
+  source                    = "github.com/canada-ca-terraform-modules/terraform-azurerm-qna-knowledgebase?ref=20210302.1"
+  location                  = local.resource_groups_L2.IM_KB.location
+  cognitiveServicesLocation = var.cognitiveServicesLocation
+  resourceGroupName         = local.resource_groups_L2.IM_KB.name
+  prefix                    = "${local.prefix}-${var.knowledgebaseList[1].languageCode}"
+  knowledgebaseList         = var.knowledgebaseList[1].knowledgebaseLocations
+  qna_tier                  = var.qna_tier
+  qna_size                  = var.qna_size
+  search_sku                = var.search_sku
+  account_sku               = var.account_sku
+  tags                      = var.tags
+  plan_id                   = azurerm_app_service_plan.Chatbot-svcplan.id
+  search_service            = var.search_service_fr
+  search_service_key        = var.search_service_key_fr
+}
+
--- L2_blueprint_Telcom-KB/environments/dev.tfvars
+++ L2_blueprint_Telcom-KB/environments/dev.tfvars
@@ -126,11 +126,7 @@ knowledgebaseList = [
 
 L2_RG_RBAC = {
   contributorNames = [
-<<<<<<< HEAD
-    "john.nephin@ssc-spc.gc.ca"
-=======
      "john.nephin@ssc-spc.gc.ca",  "stephane.robinson@ssc-spc.gc.ca", "alain.goyette@ssc-spc.gc.ca", "don.aker@ssc-spc.gc.ca"
->>>>>>> f5265a6b3dc87264297ffa91c2b2a3e2fce24eca
   ]
   ownerNames = [
     "john.nephin@ssc-spc.gc.ca",
@@ -140,11 +136,7 @@ L2_RG_RBAC = {
 
 qna_tier = "Shared"
 
-<<<<<<< HEAD
-qna_size = "D1"
-=======
 qna_size = "B1"
->>>>>>> f5265a6b3dc87264297ffa91c2b2a3e2fce24eca
 
 search_sku = "standard"
 
@@ -171,10 +163,4 @@ appPassword ="GBn.mrT6I~8pKhx7L.an53P-9BR1n_gy6f"
 search_service_en = "scdcciochatbotensvc5g6r6a9sqfvq-ss"
 search_service_key_en = "1162FEBF9E62F2E874EB764684F2BE22"
 search_service_fr = "scdcciochatbotfrsvcyby9rbgy64rt-ss"
-<<<<<<< HEAD
 search_service_key_fr = "2220F77EC936A3FD91DC080810437DE1"
-
-app_service_plan_id = "/subscriptions/6425f0ed-3443-4139-8361-9b8d3951d43e/resourceGroups/ScDc-CIO_Chatbot_Project-rg/providers/Microsoft.Web/serverfarms/ScDc-CIO-Chatbot-svcplan"
-=======
-search_service_key_fr = "2220F77EC936A3FD91DC080810437DE1"
->>>>>>> f5265a6b3dc87264297ffa91c2b2a3e2fce24eca
